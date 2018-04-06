b33aaf314048cf257568cd6e760c36eaf109b462
Tom Nielsen <tanielsen@gmail.com>
2018-04-06 19:19:42 +0100

    basics

---
+++ Stancon.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -F -pgmF inlitpp #-}
+
+```html_header
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+```
+
+
+```haskell top hide
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE NoMonomorphismRestriction  #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE Rank2Types                 #-}
+
+import           Graphics.Plotly
+
+main :: IO ()
+```
