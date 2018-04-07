bdad6c9b0340f1c4f9b292dc6d520299f7d5990d
Tom Nielsen <tanielsen@gmail.com>
2018-04-07 20:49:54 +0100

    model

--- Stancon.hs
+++ Stancon.hs
@@ -7,11 +7,60 @@
 
 ```haskell top hide
 {-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE NoMonomorphismRestriction  #-}
 {-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE Rank2Types                 #-}
 
-import           Graphics.Plotly
+import Graphics.Plotly
+import Stan.AST
+import Stan.Data
+import Stan.Run
+import Numeric.Datasets.BostonHousing
+import Numeric.Datasets
+import Lens.Micro
+import Data.Monoid ((<>))
+```
 
-main :: IO ()
+
+```haskell top
+linRegression :: [Stan]
+linRegression = [
+  Data [ lower 0 Int ::: "n"
+       , lower 0 Int ::: "p"
+       , Real ::: "y"!["n"]
+       , Real ::: "x"!["n","p"]
+       ],
+  Parameters [
+       Real ::: "beta"!["p"],
+       Real ::: "sigma"
+       ],
+  Model [
+           "beta" :~ normal (0,1)
+          ,"sigma" :~ gamma (1,1)
+          ,For "i" 1 "n" [
+            "y"!["i"] :~ normal (("x"!["i"] `dot` "beta"), "sigma")
+          ]
+        ]
+  ]
+
+getRow b = [rooms b, crimeRate b]
+```
+
+```haskell do
+bh <- getDataset bostonHousing
+
+let sdata = "y" <~ map medianValue bh <>
+            "x" <~ map getRow bh<>
+            "n" <~ length bh <>
+            "p" <~ length (getRow $ head bh)
+
+res <- runStan linRegression sdata sample {numSamples = 100}
+```
+
+
+```haskell eval
+plotly "bh" [points (aes & x .~ rooms & y .~ medianValue) bh]
+```
+
+
+```haskell eval
+show res
 ```
