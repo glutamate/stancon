b101ee7cb7c6ca4618877d4586c856e8f763bb68
Tom Nielsen <tanielsen@gmail.com>
2018-04-11 19:55:58 +0100

    posterior plot row

--- Stancon.hs
+++ Stancon.hs
@@ -10,6 +10,7 @@
 {-# LANGUAGE OverloadedStrings          #-}
 
 import Graphics.Plotly
+import Graphics.Plotly.Histogram
 import Stan.AST
 import Stan.Data
 import Stan.Run
@@ -17,6 +18,12 @@ import Numeric.Datasets.BostonHousing
 import Numeric.Datasets
 import Lens.Micro
 import Data.Monoid ((<>))
+import Data.Maybe
+import Data.Text (unpack)
+import Lucid
+import Lucid.Bootstrap
+import Lucid.Bootstrap3
+import qualified Data.Map.Strict as Map
 ```
 
 
@@ -42,6 +49,10 @@ linRegression = [
   ]
 
 getRow b = [rooms b, crimeRate b]
+
+
+postPlotRow post vnms = row_ $ rowEven MD $ flip map vnms $ \vnm -> toHtml (plotly vnm [histogram 50 $ fromJust $ Map.lookup (unpack vnm) post]
+   & (layout . margin) ?~ thinMargins & (layout . height) ?~ 300)
 ```
 
 ```haskell do
@@ -53,11 +64,16 @@ let sdata = "y" <~ map medianValue bh <>
             "p" <~ length (getRow $ head bh)
 
 res <- runStan linRegression sdata sample {numSamples = 100}
+let resEnv = mcmcToEnv res
 ```
 
 
 ```haskell eval
 plotly "bh" [points (aes & x .~ rooms & y .~ medianValue) bh]
+```
+
+```haskell eval
+postPlotRow res ["beta.1", "beta.2" ] :: Html ()
 ```
 
 
